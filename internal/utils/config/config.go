11189d3b5add3758372abd7e382480221f7a3983
Syarif Ubaidillah <ubed56pb@gmail.com>
2022-12-12 06:20:17 +0000

    FIX: configuration error for int64 types

--- internal/utils/config/config.go
+++ internal/utils/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 
 	"github.com/integralist/go-findroot/find"
 	"github.com/spf13/viper"
@@ -46,37 +48,72 @@ func LoadConfig() {
 		fmt.Println("Config file not found, using os env.")
 	}
 
-	SERVER_PORT = checkStringEnv(os.Getenv("PORT"), viper.GetString("server.PORT"))
-	MONGODB_STRING = checkStringEnv(os.Getenv("MONGODB_STRING"), viper.GetString("mongo.STRING"))
-	MONGODB_DATABASE = checkStringEnv(os.Getenv("MONGODB_DATABASE"), viper.GetString("mongo.MONGODB_DATABASE"))
-	DB_DRIVER = checkStringEnv(os.Getenv("DB_DRIVER"), viper.GetString("db.DRIVER"))
-	DB_DATABASE = checkStringEnv(os.Getenv("DB_DATABASE"), viper.GetString("db.DATABASE"))
-	DB_USERNAME = checkStringEnv(os.Getenv("DB_USERNAME"), viper.GetString("db.USERNAME"))
-	DB_PASSWORD = checkStringEnv(os.Getenv("DB_PASSWORD"), viper.GetString("db.PASSWORD"))
-	DB_HOST = checkStringEnv(os.Getenv("DB_HOST"), viper.GetString("db.HOST"))
-	DB_PORT = checkStringEnv(os.Getenv("DB_PORT"), viper.GetString("db.PORT"))
-	DB_TIMEZONE = checkStringEnv(os.Getenv("DB_TIMEZONE"), viper.GetString("db.TIMEZONE"))
-	ACCESS_KEY = []byte(checkStringEnv(os.Getenv("ACCESS_KEY"), viper.GetString("secret.ACCESS_KEY")))
-	RESET_KEY = []byte(checkStringEnv(os.Getenv("RESET_KEY"), viper.GetString("secret.RESET_KEY")))
-	SMTP_SERVER = checkStringEnv(os.Getenv("SMTP_SERVER"), viper.GetString("smtp.SERVER"))
-	SMTP_PORT = checkStringEnv(os.Getenv("SMTP_PORT"), viper.GetString("smtp.PORT"))
-	EMAIL = checkStringEnv(os.Getenv("EMAIL"), viper.GetString("smtp.EMAIL"))
-	PASSWORD = checkStringEnv(os.Getenv("PASSWORD"), viper.GetString("smtp.PASSWORD"))
-	JWT_ACCESS_EXPIRE_TIME = checkInt64Env(os.Getenv("JWT_ACCESS_EXPIRE_TIME"), viper.GetInt64("jwt.ACCESS_TIME"))
-	JWT_REFRESH_EXPIRE_TIME = checkInt64Env(os.Getenv("JWT_REFRESH_EXPIRE_TIME"), viper.GetInt64("jwt.REFRESH_TIME"))
-	JWT_RESET_PASSWORD_EXPIRE_TIME = checkInt64Env(os.Getenv("JWT_RESET_PASSWORD_EXPIRE_TIME"), viper.GetInt64("jwt.FORGOT_PASSWORD_TIME"))
+	SERVER_PORT, _ = checkEnv(os.Getenv("PORT"), viper.GetString("server.PORT"))
+	MONGODB_STRING, _ = checkEnv(os.Getenv("MONGODB_STRING"), viper.GetString("mongo.STRING"))
+	MONGODB_DATABASE, _ = checkEnv(os.Getenv("MONGODB_DATABASE"), viper.GetString("mongo.MONGODB_DATABASE"))
+	DB_DRIVER, _ = checkEnv(os.Getenv("DB_DRIVER"), viper.GetString("db.DRIVER"))
+	DB_DATABASE, _ = checkEnv(os.Getenv("DB_DATABASE"), viper.GetString("db.DATABASE"))
+	DB_USERNAME, _ = checkEnv(os.Getenv("DB_USERNAME"), viper.GetString("db.USERNAME"))
+	DB_PASSWORD, _ = checkEnv(os.Getenv("DB_PASSWORD"), viper.GetString("db.PASSWORD"))
+	DB_HOST, _ = checkEnv(os.Getenv("DB_HOST"), viper.GetString("db.HOST"))
+	DB_PORT, _ = checkEnv(os.Getenv("DB_PORT"), viper.GetString("db.PORT"))
+	DB_TIMEZONE, _ = checkEnv(os.Getenv("DB_TIMEZONE"), viper.GetString("db.TIMEZONE"))
+	// access_key, _ := checkEnv(os.Getenv("ACCESS_KEY"), viper.GetString("secret.ACCESS_KEY"))
+	// reset_key, _ := checkEnv(os.Getenv("RESET_KEY"), viper.GetString("secret.RESET_KEY"))
+	// ACCESS_KEY, _ = []byte(access_key)
+	// RESET_KEY, _ = []byte(reset_key)
+	SMTP_SERVER, _ = checkEnv(os.Getenv("SMTP_SERVER"), viper.GetString("smtp.SERVER"))
+	SMTP_PORT, _ = checkEnv(os.Getenv("SMTP_PORT"), viper.GetString("smtp.PORT"))
+	EMAIL, _ = checkEnv(os.Getenv("EMAIL"), viper.GetString("smtp.EMAIL"))
+	PASSWORD, _ = checkEnv(os.Getenv("PASSWORD"), viper.GetString("smtp.PASSWORD"))
+	_, JWT_ACCESS_EXPIRE_TIME = checkEnv(
+		os.Getenv("JWT_ACCESS_EXPIRE_TIME"), 
+		viper.GetString("jwt.ACCESS_TIME"), 
+		"int",
+	)
+	_, JWT_REFRESH_EXPIRE_TIME = checkEnv(
+		os.Getenv("JWT_REFRESH_EXPIRE_TIME"),
+		viper.GetString("jwt.REFRESH_TIME"),
+		"int",
+	)
+	_, JWT_RESET_PASSWORD_EXPIRE_TIME = checkEnv(
+		os.Getenv("JWT_RESET_PASSWORD_EXPIRE_TIME"),
+		viper.GetString("jwt.FORGOT_PASSWORD_TIME"),
+		"int",
+	)
 }
 
-func checkStringEnv(env, viperStr interface{}) string {
-	if env != "" {
-		return env.(string)
-	}
-	return viperStr.(string)
-}
+func checkEnv(args ...interface{}) (envStr string, envInt int64) {
+	for i, v := range args {
+		switch i {
+		case 0:
+			if reflect.TypeOf(v).String() == "string" {
+				envStr = v.(string)
+			} else {
+				envInt = v.(int64)
+			}
+		case 1:
+			if envStr != "" || envInt != 0 {
+				return
+			}
 
-func checkInt64Env(env, viperStr interface{}) int64 {
-	if env != "" {
-		return env.(int64)
+			if reflect.TypeOf(v).String() == "string" {
+				envStr = v.(string)
+			} else {
+				envInt = v.(int64)
+			}
+		case 2:
+			if reflect.TypeOf(v).String() != "string" {
+				return
+			}
+
+			if v == "int" || v == "int64" {
+				intEnv, _ := strconv.Atoi(envStr)
+				envInt = int64(intEnv)
+				envStr = ""
+			}
+		}
 	}
-	return viperStr.(int64)
+
+	return
 }
